100c0d0014f6211ee89d8d65dcad6051ad3e971c
klaas8 <132535742+klaas8@users.noreply.github.com>
2026-01-03 23:30:26 +0800

    更新 b.py

--- b.py
+++ b.py
@@ -1,4 +1,4 @@
-import os, json, re, requests
+import os, json, re, requests, ipaddress, time
 
 headers = {
     "user-agent": "Mozilla/5.0 (Linux; Android 12; 22021211RC Build/SKQ1.211006.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.135 Mobile Safari/537.36",
@@ -9,6 +9,7 @@ headers = {
 }
 session = requests.session()
 session.headers.update(headers)
+ips = set()
 
 def fromJson(text, variable_name="fpsList"):
     patterns = [
@@ -30,30 +31,59 @@ def fromJson(text, variable_name="fpsList"):
                     pass
     return None
 
-def save_domain(domain):
+def save():
+    fileName = "ips.txt"
     try:
-        with open(DOMAIN_FILE, 'w', encoding='utf-8') as f:
-            f.write(domain)
+        with open(fileName, "w", encoding="utf-8") as f:
+            f.write("\n".join(ips))
         try:
             os.system('git config --local user.name "github-actions[bot]" >/dev/null 2>&1')
             os.system('git config --local user.email "github-actions[bot]@users.noreply.github.com" >/dev/null 2>&1')
-            if os.system(f'git add "{DOMAIN_FILE}" >/dev/null 2>&1') == 0:
+            if os.system(f'git add "{fileName}" >/dev/null 2>&1') == 0:
                 os.system('git commit -m "更新" >/dev/null 2>&1')
                 os.system('git push --quiet --force-with-lease')
         except Exception as e:
-            return False
-        return True
+            pass
     except Exception as e:
+        pass
+
+def validate_ip_port(ip, port):
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+        if ip_obj.is_multicast or ip_obj.is_unspecified:
+            return False
+    except ValueError:
         return False
+    ip_parts = ip.split('.')
+    for part in ip_parts:
+        if not 0 <= int(part) <= 255:
+            return False
+    if not 1 <= int(port) <= 65535:
+        return False
+    return True
 
-url = "https://www.kuaidaili.com/free/inha/1"
-resp = session.get(url)
-resp.encoding = resp.apparent_encoding
+def pac(page):
+    url = f"https://www.kuaidaili.com/free/inha/{page}"
+    resp = session.get(url)
+    resp.encoding = resp.apparent_encoding
+    isOk = False
+    if resp.ok:
+        fpsList = fromJson(resp.text)
+        if fpsList:
+            for t in fpsList:
+                if not 'ip' in t or not 'port' in t: continue
+                ip = t.get('ip', "")
+                port = t.get('port', "")
+                isIp = validate_ip_port(ip, port)
+                if not isIp: continue
+                u =  f"{ip}:{port}"
+                ips.add(u)
+                isOk = True
+    return isOk
 
-if resp.ok:
-    s = fromJson(resp.text)
-    if s:
-        for t in s:
-            if not 'ip' in t or not 'port' in t: continue
-            u = t.get('ip', "") + ":" + t.get('port', "")
-            print(u)
+if pac(1):
+    time.sleep(2)
+    pac(2)
+    pass
+
+if ips: save()
